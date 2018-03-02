9206c017b926dac6bb26ef10141740b0a5a4ee6c
Oskar Szydlowski <oskar@tooploox.com>
2018-03-02 14:33:20 +0100

    Remove mocked data

--- AIM/AIM/Presentation/Presenters/TrackListPresenter.swift
+++ AIM/AIM/Presentation/Presenters/TrackListPresenter.swift
@@ -16,7 +16,7 @@ class TrackListPresenter {
     private let connector: TrackListConnector
     private let useCaseFactory: UseCaseProducing
     
-    private var onAirInfo: OnAirInfo? = OnAirInfo(stationInfo: StationInfo(name: "Mid Dawns", description: "Stay up late with our mid dawn team as they play you new music all night long.", time: "2018-03-01T01:00:00+11:00", duration: "05:00:00", presenter: "", displayTime: "300", imageURL: URL(string: "http://www.abc.net.au/triplej/programs/img/2017/middawns/background320.jpg")!), tracks: [Track(title: "Chasing Stars {Ft. Bertie Blackman}", artist: "Alice Ivy", album: "I'm Dreaming", playTime: "2018-03-01T03:19:06+11:00", duration: "00:03:39", imageURL: URL(string: "http://www.abc.net.au/triplej/albums/56545/covers/190.jpg")!, status: .playing, type: .song)])
+    private var onAirInfo: OnAirInfo?
     
     init(connector: TrackListConnector, useCaseFactory: UseCaseProducing) {
         self.connector = connector
