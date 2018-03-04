22670e7f82d2e5ceba6f1ba35655521ad088d748
Oskar Szydlowski <oskar@tooploox.com>
2018-03-04 16:02:56 +0100

    Convert TrackListView to Obj-C

---
+++ AIM/AIM/Presentation/View Protocols/TrackListView.h
@@ -0,0 +1,13 @@
+//
+//  TrackListView.h
+//  AIM
+//
+//  Created by Oskar Szydlowski on 04.03.2018.
+//  Copyright © 2018 Oskar Szydlowski. All rights reserved.
+//
+
+@protocol TrackListView
+
+- (void)refreshView;
+
+@end
