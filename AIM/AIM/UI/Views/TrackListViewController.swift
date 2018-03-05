80d4a1a20b7f43e3a02ed674edc392d7a7a40d3d
Oskar Szydlowski <oskar@tooploox.com>
2018-03-05 12:13:24 +0100

    Remove redundant method

--- AIM/AIM/UI/Views/TrackListViewController.swift
+++ AIM/AIM/UI/Views/TrackListViewController.swift
@@ -66,10 +66,6 @@ class TrackListViewController: UIViewController, UITableViewDelegate, UITableVie
     
     //MARK: - UITableViewDataSource
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return presenter.numberOfTracks()
     }
